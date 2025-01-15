85e682f5e1055880885d32b173a7288946b9e397
WALM <lbhnik12@gmail.com>
2025-01-16 02:40:34 +0400

    Add files via upload

--- image/black/black.py
+++ image/black/black.py
@@ -5,3 +5,131 @@ text = 'white'
 hover = 'grey'
 scrollable = 'white'
 scrollbar_fg = '#080808'
+
+#Система размещения кнопок. ['x', 'y']
+background_position = [1,1]
+frame_position = [20, 20]
+android_position = [30, 70]
+android_phone_status_position = [30, 370]
+phone_vendor_model_position = [20, 467]
+select_phone_model_button_position = [385, 25]
+test_state_button_position = [695, 73]
+gsi_menu_button_position = [740, 25]
+menu_button_position = [695, 25]
+firmware_image_frame_position = [385, 73]
+select_firmware_folder_button_position = [385, 470]
+forum_firmwares_forpda_button_position = [695, 276]
+phone_reboot_button_position = [695, 324]
+flash_phone_button_position = [695, 372]
+about_button_position = [695, 420]
+exit_button_position = [695, 470]
+
+firmwares_frame_position = [285, 103]
+volume_on_button_position = [10, 10]
+volume_off_button_position = [195, 10]
+close_button_position = [795, 470]
+location_music_button_position = [10, 70]
+install_firmware_complect_button_position = [10, 130]
+select_language_button_position = [10, 190]
+select_theme_button_position = [195, 190]
+get_music_name_button_position = [10, 250]
+
+hadefirm_position = [100, 100]
+progressbar_position = [100, 200]
+
+firmwares_frame_position = [285, 131]
+
+hader_state_position = [80, 110]
+phone_status_through_adb_position = [300, 200]
+phone_status_through_fastboot_position = [500, 202]
+flash_custom_partition_button_position = [40, 110]
+approve_custom_load_button_position = [40, 163]
+wipe_data_button_position = [40, 216]
+delete_product_button_position = [40, 269]
+search_gsi_button_position = [580, 110]
+forum_nonfirmwares_forpda_button_position = [580, 163]
+adbsideload_button_position = [580, 226]
+customboot_button_position = [40, 322]
+
+walmfast_logo_position = [120, 100]
+program_name_position = [400, 100]
+model_branch_position = [400, 150]
+made_by_position = [400, 200]
+extended_button_position = [400, 270]
+update_button_position = [615, 270]
+
+customboot_hader_position = [40, 30]
+customboot_descryption_position = [40, 120]
+
+donatos_destination_position = [120, 115]
+donatos_me_position = [400, 100]
+destination_and_number_card = [400, 150]
+
+langswitcher_hader_position = [5, 100]
+russian_language_position = [300, 200]
+english_language_position = [500, 202]
+
+themeswitcher_hader_position = [5, 100]
+themes_frame_position = [285, 180]
+
+gsi_hader_position = [40, 30]
+gsi_descryption_position = [40, 120]
+select_gsi_button_position = [795, 250]
+
+vbmeta_hader_position = [40, 30]
+vbmeta_descryption_position = [40, 120]
+select_vbmeta_button_position = [795, 250]
+
+update_hader_position = [0, 220]
+
+adb_sideload_hader_position = [40, 30]
+adb_sideload_descryption_position = [40, 120]
+select_adb_sideload_button_position = [795, 300]
+
+delete_product_hader_position = [40, 30]
+delete_product_descryption_position = [40, 120]
+delete_product_confirm_button_position = [175, 300]
+
+wipe_data_hader_position = [40, 30]
+wipe_data_descryption_position = [40, 120]
+wipe_data_confirm_button_position = [175, 300]
+
+flash_phone_hader_position = [0, 130]
+progressbar_fp_position = [100, 250]
+
+progressbar_re_position = [100, 200]
+hader_re_position = [80, 110]
+reboot_through_adb = [300, 200]
+reboot_through_fastboot = [500, 202]
+
+pbrp_button_position = [350, 470]
+orangefox_button_position = [20, 470]
+flashit_partition_attetion_position = [0, 10]
+select_recovery_button_position = [795, 250]
+flash_recovery_button_position = [530, 472]
+recovery_yes_button_position = [640, 340]
+recovery_no_button_position = [790, 340]
+flash_recovery_process_hader_position = [50, 340]
+
+gsi_name_position = [40, 250]
+flash_gsi_position = [585, 470]
+
+reboot_fastbotd_yes_button_position = [650, 360]
+reboot_fastbotd_no_button_position = [795, 360]
+flash_gsi_process_hader_position = [40, 356]
+
+vbmeta_name_position = [40, 250]
+flash_vbmeta_position = [550, 471]
+
+reboot_fastbot_yes_button_position = [650, 360]
+reboot_fastbot_no_button_position = [795, 360]
+flash_vbmeta_process_hader_position = [40, 356]
+
+update_yes_button_position = [300, 300]
+update_no_button_position = [500, 300]
+
+#Система отступа для списков - ['отступ x', 'отступ y']
+reboot_into_recovery_pad = [10, 5]
+reboot_into_bootloader_pad = [10, 5]
+reboot_into_fastbootd_pad = [10, 5]
+reboot_into_system_pad = [10, 5]
